9eb49697c122c65af395782c874c7b20dae10dc6
Martin Schade <schadix@gmail.com>
2015-02-08 20:55:13 -0500

    init

---
+++ copyS3.js
@@ -0,0 +1,38 @@
+var AWS = require('aws-sdk');
+
+module.exports = function(host, port) {
+	var l = require('latency-lambda-client')(host, port);
+	return {
+		// method: src-bucket, src-key, dest-bucket, dest-key, testid, identifier
+		copyS3: function(srcBucket, srcKey, dstBucket, dstKey, testid, identifier, callback) {
+			l.sendToLatency(testid, identifier, "Start");
+			s3.getObject({
+					Bucket: srcBucket,
+					Key: srcKey
+				},
+				function upload(err, data) {
+					if (err) {
+						callback(err, null);
+					} else {
+						l.sendToLatency(testid, identifier, "Upload-start");
+						s3.putObject({
+								Bucket: dstBucket,
+								Key: dstKey,
+								Body: data.Body
+							},
+							function(err, data) {
+								if (err) {
+									callback(err, null);
+								} else {
+									l.sendToLatency(testid, identifier, "Finish");
+									callback(null, 'Successfully copied from ' + srcBucket + '/' + srcKey +
+										' and uploaded to ' + dstBucket + '/' + dstKey);
+								}
+							}
+						)
+					}
+				}
+			);
+		}
+	}
+}
